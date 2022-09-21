94cf936d092b84358a06a60fef66a20ba7fa3d61
shreedhar <shreedhar7bpatil@gmail.com>
2022-09-22 00:51:31 +0530

    Portfolio page partially done

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,10 +1,19 @@
 import React from 'react'
-import "./Wallet.css"
+import "./Portfolio.css"
 
 function Portfolio() {
     return (
         <>
-        
+        <div className="portfolio-chart">
+
+        </div>
+
+        <div className="totalinvestedbox">
+            <h2>Total invested</h2>
+            <h1>52,459.54 Fcoin</h1>
+            <h3>To see all indivisual transaction timestamps go to Wallet</h3>
+        </div>
+
         </>
     )
 
