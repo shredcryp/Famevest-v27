89b0599a79a5e1c9e5262cd6970f20c2cda87592
shreedhar <shreedhar7bpatil@gmail.com>
2022-09-22 00:41:29 +0530

    Wallet page done

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -3,35 +3,107 @@ import "./Wallet.css"
 function Wallet() {
     return (
         <>
+        <div class="wallet-leftside">
 
-<div >
-  <table style={{backgroundColor:"red"}}>
-    <tr>
-      <th>First Name</th>
-      <th>Last Name</th>
-      <th>Points</th>
-     
-    </tr>
-    <tr>
-      <td>Jill</td>
-      <td>Smith</td>
-      <td>50</td>
-      
-    </tr>
-    <tr>
-      <td>Eve</td>
-      <td>Jackson</td>
-      <td>94</td>
-      
-    </tr>
-    <tr>
-      <td>Adam</td>
-      <td>Johnson</td>
-      <td>67</td>
-      
-    </tr>
-  </table>
-</div>
+            <h3>To start investing in creators add balance to your account</h3>
+            <div className="balancebox">
+                <h2>Balance available:</h2>
+                <h1>52,459.54 Fcoins</h1>
+            </div>
+
+            <div className="enteramount">
+                <input type="text" placeholder="Enter ETH amount to add..." />
+            </div>
+
+            <div className="withdrawaddamount">
+                <div className="withdrawbutton">
+                    <button>Withdraw</button>
+                </div>
+
+                <div className="addamountbutton">
+                    <button>Add Balance</button>
+                </div>
+            </div>
+
+        </div>
+
+        <div className="wallet-rightside">
+
+            <div className="walletpart">
+                
+            </div>
+
+            <div className="balancehistory">
+                <table>
+                    <tr>
+                        <th>Timestamps</th>
+                        <th>Amount</th>
+                        <th>State</th>
+                    </tr>
+                    <tr>
+                        <td>13 days 4 hours ago</td>
+                        <td>2.54 Fcoin</td>
+                        <td>Added</td>
+                    </tr>
+                    <tr>
+                        <td>7 days 23 hours ago</td>
+                        <td>3.29 Fcoin</td>
+                        <td>Withdrawn</td>
+                    </tr>
+                    <tr>
+                        <td>13 days 4 hours ago</td>
+                        <td>2.54 Fcoin</td>
+                        <td>Added</td>
+                    </tr>
+                    <tr>
+                        <td>7 days 23 hours ago</td>
+                        <td>3.29 Fcoin</td>
+                        <td>Withdrawn</td>
+                    </tr>
+                    <tr>
+                        <td>13 days 4 hours ago</td>
+                        <td>2.54 Fcoin</td>
+                        <td>Added</td>
+                    </tr>
+                    <tr>
+                        <td>7 days 23 hours ago</td>
+                        <td>3.29 Fcoin</td>
+                        <td>Withdrawn</td>
+                    </tr>
+                    <tr>
+                        <td>13 days 4 hours ago</td>
+                        <td>2.54 Fcoin</td>
+                        <td>Added</td>
+                    </tr>
+                    <tr>
+                        <td>7 days 23 hours ago</td>
+                        <td>3.29 Fcoin</td>
+                        <td>Withdrawn</td>
+                    </tr>
+                    <tr>
+                        <td>13 days 4 hours ago</td>
+                        <td>2.54 Fcoin</td>
+                        <td>Added</td>
+                    </tr>
+                    <tr>
+                        <td>7 days 23 hours ago</td>
+                        <td>3.29 Fcoin</td>
+                        <td>Withdrawn</td>
+                    </tr>
+                    <tr>
+                        <td>13 days 4 hours ago</td>
+                        <td>2.54 Fcoin</td>
+                        <td>Added</td>
+                    </tr>
+                    <tr>
+                        <td>7 days 23 hours ago</td>
+                        <td>3.29 Fcoin</td>
+                        <td>Withdrawn</td>
+                    </tr>
+                </table>
+            </div>
+
+        </div>
         </>
     )
 
