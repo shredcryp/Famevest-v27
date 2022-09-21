d75501d16e3566b0ce9b1182d90a0c3154b9e22c
shreedhar <shreedhar7bpatil@gmail.com>
2022-09-21 23:06:27 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-
 import './App.css';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
+import Wallet from './components/Wallet';
 
 function App() {
   return (
     <>
- 
     <Navbar/>
     <Sidebar/>
+    <Wallet/>
     </>
   );
 }
---
+++ src/components/Wallet.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import "./Wallet.css"
+function Wallet() {
+    return (
+        <>
+
+<div >
+  <table style={{backgroundColor:"red"}}>
+    <tr>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Points</th>
+     
+    </tr>
+    <tr>
+      <td>Jill</td>
+      <td>Smith</td>
+      <td>50</td>
+      
+    </tr>
+    <tr>
+      <td>Eve</td>
+      <td>Jackson</td>
+      <td>94</td>
+      
+    </tr>
+    <tr>
+      <td>Adam</td>
+      <td>Johnson</td>
+      <td>67</td>
+      
+    </tr>
+  </table>
+</div>
+        </>
+    )
+
+}
+
+export default Wallet
