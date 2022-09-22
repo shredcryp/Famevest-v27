11171fec06198c31506486edb912411f130bab50
shreedhar <shreedhar7bpatil@gmail.com>
2022-09-22 09:53:06 +0530

    minute stuff done

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -30,8 +30,13 @@ function Wallet() {
         <div className="wallet-rightside">
 
             <div className="walletpart">
-                
+                <h3>Wallet address</h3>
+                <div class="walletaddressline">
+                    <h3>0xa7a3A2eE2E20D45a08d0C97c9F63805008086312</h3>
+                </div>
             </div>
+
+            <br></br>
 
             <div className="balancehistory">
                 <table>
