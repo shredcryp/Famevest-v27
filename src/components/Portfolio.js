a937b8eb61bc8913fc8e54fb4fa310fae5f4a235
shreedhar <shreedhar7bpatil@gmail.com>
2022-09-30 13:45:44 +0530

    home responsive done

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import "./Portfolio.css"
+import portfoliochart from '../images/portfoliochart.png';
 
 function Portfolio() {
     return (
         <>
-        <div className="portfolio-chart">
-
-        </div>
-
+        <img src={{portfoliochart}} alt="" />
         <div className="totalinvestedbox">
             <h2>Total invested</h2>
             <h1>52,459.54 Fcoin</h1>
