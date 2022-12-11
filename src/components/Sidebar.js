194ec418a4d94d31d7ecb24cf628133cb8f38449
shreedhar <shreedhar7bpatil@gmail.com>
2022-12-11 10:54:20 +0530

    currency selector

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,52 +9,44 @@ import CreateRoundedIcon from '@mui/icons-material/CreateRounded';
 import UnfoldMoreRoundedIcon from '@mui/icons-material/UnfoldMoreRounded';
 
 function Sidebar() {
-  return (
- 
-    <html>
-    
-        <body>
-        <div className="wrapper">
-        {/* <!-- Sidebar  --> */}
-        <nav id="sidebar">
-       
+    return (
+        <html>
+            <body>
+                <div className="wrapper">
+                    {/* <!-- Sidebar  --> */}
+                    <nav id="sidebar">
+                        <ul className="list-unstyled components">
 
-            <ul className="list-unstyled components">
-         
-                <li >
-                    <a href="/myprofile" style={{textDecoration:"none",color:"white",display:"flex", alignItems:"center", fontSize:"2rem"}}><PersonIcon style={{fontSize:"2.5rem"}}/> Profile</a>
-                </li>
-                <li>
-                    <a href="/wallet" style={{textDecoration:"none",color:"white",display:"flex", alignItems:"center", fontSize:"2rem"}}><AccountBalanceWalletRoundedIcon style={{fontSize:"2.5rem"}}/> Wallet </a>
+                            <li >
+                                <a href="/myprofile" style={{ textDecoration: "none", color: "white", display: "flex", alignItems: "center", fontSize: "2rem" }}><PersonIcon style={{ fontSize: "2.5rem" }} /> Profile</a>
+                            </li>
+                            <li>
+                                <a href="/wallet" style={{ textDecoration: "none", color: "white", display: "flex", alignItems: "center", fontSize: "2rem" }}><AccountBalanceWalletRoundedIcon style={{ fontSize: "2.5rem" }} /> Wallet </a>
 
-                </li>
-                <li>
-                    <a href="/portfolio" data-toggle="collapse" aria-expanded="false" style={{textDecoration:"none",color:"white",display:"flex", alignItems:"center", fontSize:"2rem"}}><EqualizerRoundedIcon style={{fontSize:"2.5rem"}}/>{'  '}  Portfolio</a>
-                   
-                </li>
-                <li>
-                    <a href="/watchlist" style={{textDecoration:"none",color:"white",display:"flex", alignItems:"center", fontSize:"2rem"}}> <BookmarkRoundedIcon style={{fontSize:"2.5rem"}}/> {'  '}  Watchlist</a>
-                </li>
-                <li>
-                    <a href="/inbox" style={{textDecoration:"none",color:"white",display:"flex", alignItems:"center", fontSize:"2rem"}}><MailRoundedIcon style={{fontSize:"2.5rem"}}/>{'  '}  Inbox</a>
-                </li>
-                <li>
-                    <a href="/notes" style={{textDecoration:"none",color:"white",display:"flex", alignItems:"center", fontSize:"2rem"}}><CreateRoundedIcon style={{fontSize:"2.5rem"}}/>  {'  '} Notes</a>
-                </li>
-                <li>
-                    <a href="/more" style={{textDecoration:"none",color:"white",display:"flex", alignItems:"center", fontSize:"2rem"}}><UnfoldMoreRoundedIcon style={{fontSize:"2.5rem"}}/>   More</a>
-                </li>
-            </ul>
-        </nav>
+                            </li>
+                            <li>
+                                <a href="/portfolio" data-toggle="collapse" aria-expanded="false" style={{ textDecoration: "none", color: "white", display: "flex", alignItems: "center", fontSize: "2rem" }}><EqualizerRoundedIcon style={{ fontSize: "2.5rem" }} />{'  '}  Portfolio</a>
 
+                            </li>
+                            <li>
+                                <a href="/watchlist" style={{ textDecoration: "none", color: "white", display: "flex", alignItems: "center", fontSize: "2rem" }}> <BookmarkRoundedIcon style={{ fontSize: "2.5rem" }} /> {'  '}  Watchlist</a>
+                            </li>
+                            <li>
+                                <a href="/inbox" style={{ textDecoration: "none", color: "white", display: "flex", alignItems: "center", fontSize: "2rem" }}><MailRoundedIcon style={{ fontSize: "2.5rem" }} />{'  '}  Inbox</a>
+                            </li>
+                            <li>
+                                <a href="/notes" style={{ textDecoration: "none", color: "white", display: "flex", alignItems: "center", fontSize: "2rem" }}><CreateRoundedIcon style={{ fontSize: "2.5rem" }} />  {'  '} Notes</a>
+                            </li>
+                            <li>
+                                <a href="/more" style={{ textDecoration: "none", color: "white", display: "flex", alignItems: "center", fontSize: "2rem" }}><UnfoldMoreRoundedIcon style={{ fontSize: "2.5rem" }} />   More</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </body>
+        </html>
+    )
 
-    </div>
-        </body>
-    </html>
-    
-      
-  )
-  
 }
 
 export default Sidebar
--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -1,118 +1,131 @@
 import React from 'react'
 import "./Wallet.css"
+import usd from '../images/usd.png';
 function Wallet() {
     return (
         <>
-        <div class="fullwallet" style={{display:"flex", justifyContent:"space-around"}}>
-        <div class="wallet-leftside">
+            <div class="fullwallet" style={{ display: "flex", justifyContent: "space-around" }}>
+                <div class="wallet-leftside">
 
-<h3>To start investing in creators add balance to your account</h3>
-<div className="balancebox">
-    <h2>Balance available:</h2>
-    <h1>52,459.54 Fcoins</h1>
-</div>
+                    <h3>To start investing in creators add balance to your account</h3>
+                    <div className="balancebox">
+                        <h2>Balance available:</h2>
+                        <h1>52,459.54 Fcoins</h1>
+                    </div>
 
-<div className="enteramount">
-    <input type="text" placeholder="Enter ETH amount to add..." />
-</div>
+                    <div className="enteramountcurrencyselector">
+                        <div className="enteramount">
+                            <input type="text" placeholder="Enter amount to add..." />
+                            </div>
+                            <form>
+                                <select className="currencyselector">
+                                    <option value="USD"><img src={usd} alt=""/>USD</option>
+                                    <option value="INR">INR</option>
+                                </select>
+                            </form>
+                        
+                    </div>
 
-<div className="withdrawaddamount">
-    <div className="withdrawbutton">
-        <button>Withdraw</button>
-    </div>
 
-    <div className="addamountbutton">
-        <button>Add Balance</button>
-    </div>
-</div>
 
-</div>
-<div className="wallet-rightside" style={{float:"right"}}>
+                    <div className="withdrawaddamount">
+                        <div className="withdrawbutton">
+                            <button>Withdraw</button>
+                        </div>
 
-            <div className="walletpart">
-                <h3>Wallet address</h3>
-                <div class="walletaddressline">
-                    <h3>0xa7a3A2eE2E20D45a08d0C97c9F638...</h3>
+                        <div className="addamountbutton">
+                            <button>Add Balance</button>
+                        </div>
+                    </div>
+
+                </div>
+                <div className="wallet-rightside" style={{ float: "right" }}>
+
+                    <div className="walletpart">
+                        <h3>Wallet address</h3>
+                        <div class="walletaddressline">
+                            <h3>0xa7a3A2eE2E20D45a08d0C97c9F638...</h3>
+                        </div>
+                    </div>
+
+                    <br></br>
+
+                    <div className="balancehistory">
+                        <table>
+                            <tr>
+                                <th>Timestamps</th>
+                                <th>Amount</th>
+                                <th>State</th>
+                            </tr>
+                            <tr>
+                                <td>13 days 4 hours ago</td>
+                                <td>2.54 Fcoin</td>
+                                <td>Added</td>
+                            </tr>
+                            <tr>
+                                <td>7 days 23 hours ago</td>
+                                <td>3.29 Fcoin</td>
+                                <td>Withdrawn</td>
+                            </tr>
+                            <tr>
+                                <td>13 days 4 hours ago</td>
+                                <td>2.54 Fcoin</td>
+                                <td>Added</td>
+                            </tr>
+                            <tr>
+                                <td>7 days 23 hours ago</td>
+                                <td>3.29 Fcoin</td>
+                                <td>Withdrawn</td>
+                            </tr>
+                            <tr>
+                                <td>13 days 4 hours ago</td>
+                                <td>2.54 Fcoin</td>
+                                <td>Added</td>
+                            </tr>
+                            <tr>
+                                <td>7 days 23 hours ago</td>
+                                <td>3.29 Fcoin</td>
+                                <td>Withdrawn</td>
+                            </tr>
+                            <tr>
+                                <td>13 days 4 hours ago</td>
+                                <td>2.54 Fcoin</td>
+                                <td>Added</td>
+                            </tr>
+                            <tr>
+                                <td>7 days 23 hours ago</td>
+                                <td>3.29 Fcoin</td>
+                                <td>Withdrawn</td>
+                            </tr>
+                            <tr>
+                                <td>13 days 4 hours ago</td>
+                                <td>2.54 Fcoin</td>
+                                <td>Added</td>
+                            </tr>
+                            <tr>
+                                <td>7 days 23 hours ago</td>
+                                <td>3.29 Fcoin</td>
+                                <td>Withdrawn</td>
+                            </tr>
+                            <tr>
+                                <td>13 days 4 hours ago</td>
+                                <td>2.54 Fcoin</td>
+                                <td>Added</td>
+                            </tr>
+                            <tr>
+                                <td>7 days 23 hours ago</td>
+                                <td>3.29 Fcoin</td>
+                                <td>Withdrawn</td>
+                            </tr>
+                        </table>
+                    </div>
+
                 </div>
             </div>
 
-            <br></br>
 
-            <div className="balancehistory">
-                <table>
-                    <tr>
-                        <th>Timestamps</th>
-                        <th>Amount</th>
-                        <th>State</th>
-                    </tr>
-                    <tr>
-                        <td>13 days 4 hours ago</td>
-                        <td>2.54 Fcoin</td>
-                        <td>Added</td>
-                    </tr>
-                    <tr>
-                        <td>7 days 23 hours ago</td>
-                        <td>3.29 Fcoin</td>
-                        <td>Withdrawn</td>
-                    </tr>
-                    <tr>
-                        <td>13 days 4 hours ago</td>
-                        <td>2.54 Fcoin</td>
-                        <td>Added</td>
-                    </tr>
-                    <tr>
-                        <td>7 days 23 hours ago</td>
-                        <td>3.29 Fcoin</td>
-                        <td>Withdrawn</td>
-                    </tr>
-                    <tr>
-                        <td>13 days 4 hours ago</td>
-                        <td>2.54 Fcoin</td>
-                        <td>Added</td>
-                    </tr>
-                    <tr>
-                        <td>7 days 23 hours ago</td>
-                        <td>3.29 Fcoin</td>
-                        <td>Withdrawn</td>
-                    </tr>
-                    <tr>
-                        <td>13 days 4 hours ago</td>
-                        <td>2.54 Fcoin</td>
-                        <td>Added</td>
-                    </tr>
-                    <tr>
-                        <td>7 days 23 hours ago</td>
-                        <td>3.29 Fcoin</td>
-                        <td>Withdrawn</td>
-                    </tr>
-                    <tr>
-                        <td>13 days 4 hours ago</td>
-                        <td>2.54 Fcoin</td>
-                        <td>Added</td>
-                    </tr>
-                    <tr>
-                        <td>7 days 23 hours ago</td>
-                        <td>3.29 Fcoin</td>
-                        <td>Withdrawn</td>
-                    </tr>
-                    <tr>
-                        <td>13 days 4 hours ago</td>
-                        <td>2.54 Fcoin</td>
-                        <td>Added</td>
-                    </tr>
-                    <tr>
-                        <td>7 days 23 hours ago</td>
-                        <td>3.29 Fcoin</td>
-                        <td>Withdrawn</td>
-                    </tr>
-                </table>
-            </div>
 
-        </div>
-        </div>
-    
 
-        
         </>
     )
 
